9ba7a2a59c34e12ee0b811200d92855fa6f8778e
saleem8396 <saleemraja8396@gmail.com>
2022-01-31 07:46:25 -0800

    other code

--- src/test/java/org/github/testcase.java
+++ src/test/java/org/github/testcase.java
@@ -2,10 +2,9 @@ package org.github;
 
 public class testcase {
 	public static void main(String[] args) {
-		System.out.println("tst1");
-		System.out.println("test2");
+		
 		System.out.println("test4");
-		System.out.println("teSt3");
+		System.out.println("test5");
 		
 	}
 
